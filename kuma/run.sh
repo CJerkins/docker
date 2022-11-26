354fce7161122aa1117c840f4932d84fe96907e8
CJerkins <fortified.tech.advisor@gmail.com>
2022-11-25 21:52:59 -0500

    update

--- kuma/run.sh
+++ kuma/run.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-sudo docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1
+docker run -d --restart=always -p 3001:3001 -v /var/run/docker.sock:/var/run/docker.sock -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1
+
